805550058d0e7bb0cdc4623c1c6ea5375d4fbeb4
Sergio Revilla <sergio@unmundotecnologico.org>
2013-10-30 18:32:52 +0100

    Initial commit

---
+++ localstorage-hack/localstorage.js
@@ -0,0 +1,11 @@
+var movie_database = {
+    create: function(name, director, year, genre) {
+        
+        debugger
+        window.localStorage.setITem["movies", name];
+    }
+}
+
+
+
+movie.create("Batman", "Tim Burton", 1989, "Action" );
