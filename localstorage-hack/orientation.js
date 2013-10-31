8bc29c5fb7b167ccc6919eac7647e6cb7c1837fd
Sergio Revilla <sergio@unmundotecnologico.org>
2013-10-31 22:29:11 +0100

    Creating js skeleton for device orientation

--- localstorage-hack/orientation.js
+++ localstorage-hack/orientation.js
@@ -1,3 +1,16 @@
 if (window.DeviceOrientationEvent) {
   console.log("DeviceOrientation are supported!");
 }
+
+function onChange(eventData) {
+    var tiltLR = eventData.gamma;
+    var tiltFP = eventData.beta;
+    var direction = eventData.alpha;
+
+    console.log('tiltLR data: ' + tiltLR);
+    console.log('tiltFP data: ' + tiltFP);
+    console.log('direction data: ' + direction);
+}
+
+window.addEventListener('DeviceOrientation', onChange, false);
+
